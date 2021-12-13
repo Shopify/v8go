d53f67fcdbdd5dc6f1af53d9e400bd1c5bf1bd22
Genevieve L'Esperance <genevieve.lesperance@shopify.com>
2021-12-13 14:52:27 -0800

    WIP bench

---
+++ perf_test.go
@@ -0,0 +1,82 @@
+package v8go_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"rogchap.com/v8go"
+)
+
+func BenchmarkProfiler(b *testing.B) {
+	b.ReportAllocs()
+
+	vm := v8go.NewIsolate()
+	profiler := v8go.NewCPUProfiler(vm)
+
+	ctx := v8go.NewContext(vm)
+	_, _ = ctx.RunScript(profileScript, "script.js")
+	val, _ := ctx.Global().Get("start")
+	fn, _ := val.AsFunction()
+
+	for n := 0; n < b.N; n++ {
+		profileName := fmt.Sprintf("test-%d", n)
+		profiler.StartProfiling(profileName)
+
+		_, _ = fn.Call(ctx.Global())
+
+		start := time.Now()
+		profile := profiler.StopProfiling(profileName)
+		printTree(profile.GetTopDownRoot())
+		fmt.Println(time.Since(start))
+
+		profile.Delete()
+	}
+
+	ctx.Close()
+	profiler.Dispose()
+	vm.Dispose()
+}
+
+func TestProfilerPerf(t *testing.T) {
+	fmt.Println("starting test")
+	// rounds := 1000
+	// for n := 0; n < rounds; n++ {
+	vm := v8go.NewIsolate()
+	ctx := v8go.NewContext(vm)
+	profiler := v8go.NewCPUProfiler(vm)
+
+	profileName := fmt.Sprintf("test-%d", 0)
+
+	profiler.StartProfiling(profileName)
+	ctx.RunScript(profileScript, "script.js")
+	val, _ := ctx.Global().Get("start")
+	fn, _ := val.AsFunction()
+	fn.Call(ctx.Global())
+
+	start := time.Now()
+	profile := profiler.StopProfiling(profileName)
+	printTree(profile.GetTopDownRoot())
+	fmt.Printf(" duration to generate profile %s\n", time.Since(start))
+
+	profile.Delete()
+	profiler.Dispose()
+	ctx.Close()
+	vm.Dispose()
+	// }
+
+}
+
+// Note that to focus on exercising each function, we skip printing
+// but printing is useful to just double-check the tree looks correct.
+func printTree(node *v8go.CPUProfileNode) {
+	// fmt.Printf("%s %s:%d:%d\n", node.GetFunctionName(), node.GetScriptResourceName(), node.GetLineNumber(), node.GetColumnNumber())
+	node.GetFunctionName()
+	node.GetLineNumber()
+	node.GetColumnNumber()
+	node.GetScriptResourceName()
+	count := node.GetChildrenCount()
+	for i := 0; i < count; i++ {
+		printTree(node.GetChild(i))
+	}
+}
