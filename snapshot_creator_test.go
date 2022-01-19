0ca3638036a627ff19d89fcca9af69265dd91754
Gustavo Caso <gustavocaso@gmail.com>
2022-01-19 14:43:11 +0100

    Fix typos

--- snapshot_creator_test.go
+++ snapshot_creator_test.go
@@ -13,12 +13,12 @@ import (
 func TestCreateSnapshot(t *testing.T) {
 	snapshotCreator := v8.NewSnapshotCreator()
 	snapshotCreatorIso := snapshotCreator.GetIsolate()
-	snapshotCreatoCtx := v8.NewContext(snapshotCreatorIso)
-	defer snapshotCreatoCtx.Close()
+	snapshotCreatorCtx := v8.NewContext(snapshotCreatorIso)
+	defer snapshotCreatorCtx.Close()
 
-	snapshotCreatoCtx.RunScript(`const add = (a, b) => a + b`, "add.js")
-	snapshotCreatoCtx.RunScript(`function run() { return add(3, 4); }`, "main.js")
-	err := snapshotCreator.AddContext(snapshotCreatoCtx)
+	snapshotCreatorCtx.RunScript(`const add = (a, b) => a + b`, "add.js")
+	snapshotCreatorCtx.RunScript(`function run() { return add(3, 4); }`, "main.js")
+	err := snapshotCreator.AddContext(snapshotCreatorCtx)
 	fatalIf(t, err)
 
 	data, err := snapshotCreator.Create(v8.FunctionCodeHandlingKlear)
@@ -52,19 +52,19 @@ func TestCreateSnapshot(t *testing.T) {
 func TestCreateSnapshotErrorAfterSuccessfullCreate(t *testing.T) {
 	snapshotCreator := v8.NewSnapshotCreator()
 	snapshotCreatorIso := snapshotCreator.GetIsolate()
-	snapshotCreatoCtx := v8.NewContext(snapshotCreatorIso)
-	defer snapshotCreatoCtx.Close()
+	snapshotCreatorCtx := v8.NewContext(snapshotCreatorIso)
+	defer snapshotCreatorCtx.Close()
 
-	snapshotCreatoCtx.RunScript(`const add = (a, b) => a + b`, "add.js")
-	snapshotCreatoCtx.RunScript(`function run() { return add(3, 4); }`, "main.js")
-	err := snapshotCreator.AddContext(snapshotCreatoCtx)
+	snapshotCreatorCtx.RunScript(`const add = (a, b) => a + b`, "add.js")
+	snapshotCreatorCtx.RunScript(`function run() { return add(3, 4); }`, "main.js")
+	err := snapshotCreator.AddContext(snapshotCreatorCtx)
 	fatalIf(t, err)
 
 	data, err := snapshotCreator.Create(v8.FunctionCodeHandlingKlear)
 	fatalIf(t, err)
 	defer data.Dispose()
 
-	err = snapshotCreator.AddContext(snapshotCreatoCtx)
+	err = snapshotCreator.AddContext(snapshotCreatorCtx)
 	if err == nil {
 		t.Error("Adding context should have fail")
 	}
